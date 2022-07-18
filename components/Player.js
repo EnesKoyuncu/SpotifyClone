3cd1eedd2d8750ac1cf179e9ef45319d868c7063
Enes Ertuğrul Koyuncu <enes.koyuncu5507@gmail.com>
2022-07-19 02:37:40 +0300

    ses arttırma ve azaltma tuşlarının fonksiyonları halledildi

--- components/Player.js
+++ components/Player.js
@@ -52,7 +52,7 @@ function Player () {
 
   return (
     <div className='h-24 bg-gradient-to-b from-black to-gray-900
-     text-white grid grid-cols-3 text-xs md:text-base px-2 md:px-8'>
+      text-white grid grid-cols-3 text-xs md:text-base px-2 md:px-8'>
         {/* Left */}
         <div className='flex items-center space-x-4'>
             <img className='hidden md:inline h-10 w10' 
@@ -79,9 +79,18 @@ function Player () {
               <FastForwardIcon className='button' />
 
               <ReplyIcon className='button' />
+          </div>
 
-              
-
+          {/* Right */}
+          <div className='flex items-center space-x-3 md:space-x-4 justify-end pr-5'>
+            <VolumeDownIcon onClick={() => volume > 0 && setVolume(volume -10) } className='button' />
+              <input className='w-14 md:w-28' 
+              type="range" 
+              value={volume}
+              onChange= { e => setVolume( Number(e.target.value) ) }
+              min={0} 
+              max={100} />
+            <VolumeUpIcon onClick={() => volume < 100 && setVolume(volume +10) } className='button' />
           </div>
     </div>
   );
