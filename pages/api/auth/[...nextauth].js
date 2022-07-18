2ff0b5edb44c8c75796b258818aab155f2ef8ca5
Enes Ertuğrul Koyuncu <enes.koyuncu5507@gmail.com>
2022-07-19 00:14:16 +0300

    Player Kismina kadar gelindi

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,84 +1,87 @@
 import { Session } from "inspector";
-import { SessionProvider } from "next-auth/react"
 import NextAuth from "next-auth"
 import SpotifyProvider from "next-auth/providers/spotify"
-import spotifyApi, { LOGIN_URL } from "../../../lib/spotify"
+import  spotifyApi,{ LOGIN_URL } from "../../../lib/spotify"
 
 async function refreshAccessToken(token){
-  try{
 
-    spotifyApi.setAccessToken(token.accesToken);
-    spotifyApi.setRefreshToken(token.refreshToken);
+    try{
 
-    const {body: refreshedToken} = await spotifyApi.refreshAccessToken();
-    console.log("REFRESHED TOKEN IS", refreshedToken)
+        spotifyApi.setAccessToken(token.accessToken);
+        spotifyApi.setRefreshToken(token.refreshToken);
 
-    return{
-      ...token,
-      accesToken : refreshedToken.access_token,
-      accessTokenExpires : Date.now + refreshedToken.expires_in *1000, // 1 hour as 3600 returns from Spotify API
-      refreshToken : refreshedToken.refresh_token ?? token.refreshToken,
-      // replace if new one comes back else fall back to old refresh token
+        const{body:refreshedToken} = await spotifyApi.refreshAccessToken();
+        console.log("REFRESHED TOKEN IS",refreshedToken);
+0
+        return{
+            ...token,
+            accessToken:refreshedToken.access_token,
+            accessTokenExpires: Date.now + refreshToken.expires_in * 1000,
+            refreshToken: refreshedToken.refresh_token ?? token.refreshToken,
+        }
+
+    }catch(error){
+
+        console.error(error)
+        return{
+            ...token,
+            error: "RefreshAccessTokenError"
+        };
     }
 
-  }catch(error){ // basit hata yakalama olayı. c#'ta da vardı.
-    console.error(error)
 
-    return{
-      ...token,
-      error: 'RefreshAccessTokenError',
-    };
-  }
 }
-
-
 
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
-    SpotifyProvider ({
+    SpotifyProvider({
       clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
       clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
       authorization: LOGIN_URL,
     }),
     // ...add more providers here
   ],
-  secret: process.env.JWT_SECRET,
-  pages: {
-    signIn: "/login",
+  secret:process.env.JWT_SECRET,
+  pages:{
+      signIn:'/login'
   },
-
-  callbacks: {
-    async jwt({token, account, user}){
-
-        //initial sign in
-        if(account && user) {
+  callbacks:{
+    async jwt({token,account,user}){
+        //initial login
+        if(account&&user){
             return{
-                ...token,
-                accesToken: account.access_token,
-                refreshToken: account.refresh_token,
-                username: account.providerAccountId,
-                accessTokenExpires : account.expires_at *1000, 
-            }
+                
+                ... token,
+                accessToken:account.access_token,
+                refreshToken:account.refresh_token,
+                username:account.providerAccountId,
+                accessTokenExpires: account.expires_at * 1000,
+
+            };
+
         }
 
-        // return previous token if the access token has not expired yet
-        if(Date.now() < token.accessTokenExpires){
+        //return previous token
+        if(Date.now()<token.accessTokenExpires){
             console.log("EXISTING ACCESS TOKEN IS VALID");
             return token;
         }
-        
-        // access token has expired, so we need to refresh it ...
-            console.log("ACCES TOKEN HAS EXPIRED, REFRESHING...");
-            return await refreshAccessToken(token);
+        //expired token
+        console.log("ACCESS TOKEN HAS EXPIRED, REFRESHING...")
+        return await refreshAccessToken(token)
     },
+    
+    async session({session,token}){
+        
+        session.user.accessToken= token.accessToken;
+        session.user.refreshToken=token.refreshToken;
+        session.user.username=token.username;
+        return session;
+                            
 
-    async session ({session,token}){
-      session.user.accesToken = token.accesToken;
-      session.user.refreshToken = token.refreshToken;
-      session.user.username = token.username;
+    },
+  },
 
-      return session;
-    }
-  }
-})
+  
+});
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,7 @@
-import{ NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
 import Sidebar from '../components/Sidebar'
 import Center from '../components/Center'
-
+import { getSession } from 'next-auth/react';
+import Player from '../components/Player'
 
 export default function Home() {
   
@@ -15,8 +13,20 @@ export default function Home() {
         <Center/>
       </main>
       
-      <div>{/* Player */}</div>
+      <div className='sticky bottom-0'>
+        <Player />
+      </div>
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context);
+
+  return{
+    props: {
+      session
+    }
+  }
 }
 
